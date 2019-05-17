af6f621a81f19210a3b1d24fbdae3e6c4f4e7564
Ivan Szebenszki <szib77@gmail.com>
2019-05-17 08:46:08 +0100

    Done.

---
+++ app/services/sighting_serializer.rb
@@ -0,0 +1,17 @@
+class SightingSerializer
+
+  def initialize(sighting_object)
+    @sighting = sighting_object
+  end
+
+  def to_serialized_hash
+    @sighting.to_json(
+      :include => {
+        :bird => {:only =>[:name, :species]}, 
+        :location => {:only => [:latitude, :longitude]}
+      }, 
+      :except => [:updated_at]
+    )
+  end
+
+end
